5676f3337c5f74ccf5cd20501abf5fcb12db6f2a
Kryshtof Yurii <yura.krishtof@gmail.com>
2023-11-22 13:22:58 +0200

    Added questionSubmit script

---
+++ labs/bootstrap/scripts/questionSubmit.js
@@ -0,0 +1,36 @@
+// Отримання форми за допомогою getElementById
+var askQuestionForm = document.getElementById("ask-question-form");
+
+// Додавання обробника події для форми
+askQuestionForm.addEventListener("submit", function (event) {
+// Зупиняємо стандартну поведінку форми (відправку)
+event.preventDefault();
+
+// Отримання значень полів
+var nameUser = document.getElementById("name_user").value;
+var email = document.getElementById("email").value;
+var phone = document.getElementById("phone").value;
+
+// Отримання значення обраного radio
+var replyRadio;
+if (document.getElementById("emailRadio").checked) {
+    replyRadio = "E-mail";
+} else if (document.getElementById("smsRadio").checked) {
+    replyRadio = "SMS";
+}
+
+var massiveText = document.getElementById("massive_text").value;
+
+// Створення рядка для відображення у вікні alert
+var alertMessage =
+    "Your question was submitted succesfully" +
+    "\nInfo" +
+    "\nName: " + nameUser +
+    "\nEmail: " + email +
+    "\nPhone: " + phone +
+    "\nReply method: " + replyRadio +
+    "\nQuestion: " + massiveText;
+
+// Виведення значень у вікно alert
+alert(alertMessage);
+});
